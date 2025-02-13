b248702ba13ecfe91c7f4698e3d375f68dddc01b
Niek Peters <n.s.peters05@gmail.com>
2025-02-13 19:42:38 +0100

    Got platform-independant basic dragging working

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,115 @@ type DragState = {
 const draggables = new Map<HTMLElement, DragSettings>();
 const dragging = new Map<HTMLElement, DragState>();
 
+document.addEventListener("touchmove", moveTouch);
+
 export function draggable(el: HTMLElement, settings: DragSettings) {
+  if (draggables.has(el)) return;
+
   draggables.set(el, settings);
 
   el.classList.add("draggable");
+  el.draggable = true;
 
-  const startF = (e: PointerEvent) => start(el, e);
-  const moveF = (e: PointerEvent) => move(el, e);
-  const endF = (e: PointerEvent) => end(el, e);
-  el.addEventListener("pointerdown", startF);
-  el.addEventListener("pointermove", moveF);
-  el.addEventListener("pointerup", endF);
+  el.addEventListener("dragstart", startDrag);
+  el.addEventListener("mousedown", startMouse);
+  el.addEventListener("drag", moveMouse);
+  el.addEventListener("dragend", endMouse);
+  el.addEventListener("mouseup", endMouse);
+
+  el.addEventListener("touchstart", startTouch);
+  el.addEventListener("touchend", endTouch);
+  el.addEventListener("touchcancel", endTouch);
 }
 
-function start(el: HTMLElement, e: PointerEvent) {
+export function undraggable(el: HTMLElement) {
+  if (!draggables.has(el)) return;
+
+  draggables.delete(el);
+
+  el.classList.remove("draggable");
+  el.draggable = false;
+
+  el.removeEventListener("dragstart", startDrag);
+  el.removeEventListener("mousedown", startMouse);
+  el.removeEventListener("drag", moveMouse);
+  el.removeEventListener("dragend", endMouse);
+  el.removeEventListener("mouseup", endMouse);
+
+  el.removeEventListener("touchstart", startTouch);
+  el.removeEventListener("touchend", endTouch);
+  el.removeEventListener("touchcancel", endTouch);
+}
+
+function startDrag(e: DragEvent) {
+  console.log("startdrag");
+  const dataTransfer = e.dataTransfer!;
+  const canvas = document.createElement("canvas");
+  dataTransfer.effectAllowed = "move";
+  dataTransfer.setDragImage(canvas, 0, 0);
+  canvas.remove();
+}
+
+function startMouse(e: MouseEvent) {
+  console.log("startmouse");
+  const el = e.currentTarget! as HTMLElement;
+  const pos: Position = [e.clientX, e.clientY];
+
+  start(el, pos);
+}
+
+function startTouch(e: TouchEvent) {
+  console.log("starttouch");
+  e.preventDefault();
+
+  const el = e.currentTarget! as HTMLElement;
+  const current = e.targetTouches[0];
+  const pos: Position = [current.clientX, current.clientY];
+
+  start(el, pos);
+}
+
+function moveMouse(e: MouseEvent) {
+  // if (isTouch() || dragging.size === 0) return;
+  if (dragging.size === 0) return;
+  console.log("movemouse");
+
+  const el = e.currentTarget! as HTMLElement;
+  const pos: Position = [e.clientX, e.clientY];
+
+  if (pos[0] === 0 || pos[1] === 0) return;
+
+  move(el, pos);
+}
+
+function moveTouch(e: TouchEvent) {
+  if (dragging.size === 0) return;
+  console.log("movetouch");
+
+  for (const touch of e.changedTouches) {
+    const el = touch.target as HTMLElement;
+    const pos: Position = [touch.clientX, touch.clientY];
+
+    move(el, pos);
+  }
+}
+
+function endMouse(e: MouseEvent) {
+  console.log("endmouse", e instanceof DragEvent ? "drag" : "normal");
+  const el = e.currentTarget! as HTMLElement;
+
+  end(el);
+}
+
+function endTouch(e: TouchEvent) {
+  console.log("endtouch");
+  e.preventDefault();
+  const el = e.currentTarget! as HTMLElement;
+
+  end(el);
+}
+
+function start(el: HTMLElement, pos: Position) {
   const startTranslate = el.style.translate
     .split(/\s+/)
     .map((part) => parseInt(part.replace("px", "") || "0"));
@@ -31,31 +126,21 @@ function start(el: HTMLElement, e: PointerEvent) {
 
   const state: DragState = {
     startPos: [el.offsetLeft, el.offsetTop],
-    startMouse: [e.clientX, e.clientY],
+    startMouse: pos,
     startTranslate: startTranslate as [number, number],
   };
   dragging.set(el, state);
-  console.log(state.startTranslate);
 }
 
-function move(el: HTMLElement, e: PointerEvent) {
+function move(el: HTMLElement, pos: Position) {
   const state = dragging.get(el);
   if (state === undefined) return;
 
   el.style.translate = `${
-    state.startTranslate[0] + e.clientX - state.startMouse[0]
-  }px ${state.startTranslate[1] + e.clientY - state.startMouse[1]}px`;
+    state.startTranslate[0] + pos[0] - state.startMouse[0]
+  }px ${state.startTranslate[1] + pos[1] - state.startMouse[1]}px`;
 }
 
-function end(el: HTMLElement, e: PointerEvent) {
-  //   const state = dragging.get(el);
-  //   if (state === undefined) return;
-
+function end(el: HTMLElement) {
   dragging.delete(el);
 }
-
-// function getTranslateXY(el: HTMLElement): Position {
-//   const style = window.getComputedStyle(el);
-//   const matrix = new DOMMatrixReadOnly(style.transform);
-//   return [matrix.m41, matrix.m42];
-// }
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// let touch = false;
+// document.addEventListener("touchstart", (e) => {
+//   touch = true;
+// });
+// document.addEventListener("touchend", (e) => {
+//   touch = false;
+// });
+
+// export function isTouch() {
+//   return touch;
+// }
