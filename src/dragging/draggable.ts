7472089bc0511a78ad0768ebc5f8343f6f0ba707
Niek Peters <n.s.peters05@gmail.com>
2025-04-05 18:49:29 +0200

    Made ghost and z-index actually work

--- src/dragging/draggable.ts
+++ src/dragging/draggable.ts
@@ -1,6 +1,6 @@
 type Position = [number, number];
 
-type DragSettings = { ghost?: boolean; cursor?: string; bounds?: HTMLElement };
+type DragSettings = { ghost?: true; cursor?: string; bounds?: HTMLElement };
 type DragState = {
   startPos: Position;
   startMouse: Position;
@@ -13,12 +13,32 @@ const draggables = new Map<HTMLElement, DragSettings>();
 const dragging = new Map<HTMLElement, DragState>();
 let singleDragging: [HTMLElement, DragState] | undefined;
 
+const ghosts = new Map<HTMLElement, HTMLElement>();
+
+const order: [HTMLElement, number][] = [];
+let orderBase = 0;
+
 export function draggable(el: HTMLElement, settings: DragSettings = {}) {
   if (draggables.has(el)) return;
 
   draggables.set(el, settings);
 
   el.classList.add("draggable");
+
+  const zIndex = getComputedStyle(el).zIndex;
+  const z = zIndex === "auto" ? 0 : parseInt(zIndex);
+
+  order.push([el, z]);
+
+  for (let i = order.length - 1; i > 0; i--) {
+    if (order[i][1] < order[i - 1][1]) {
+      const temp = order[i - 1];
+      order[i - 1] = order[i];
+      order[i] = temp;
+    } else break;
+  }
+
+  orderBase = order[order.length - 1][1];
 
   const startMouseF = (e: MouseEvent) => startMouse(e, settings);
   const startTouchF = (e: TouchEvent) => startTouch(e, settings);
@@ -41,6 +61,8 @@ export function draggable(el: HTMLElement, settings: DragSettings = {}) {
 function startMouse(e: MouseEvent, settings: DragSettings) {
   // console.log("startmouse");
   const el = e.currentTarget! as HTMLElement;
+  if (!draggables.get(el)) return;
+
   const pos: Position = [e.clientX, e.clientY];
 
   const state = start(el, pos, settings);
@@ -60,6 +82,8 @@ function startTouch(e: TouchEvent, settings: DragSettings) {
   if (e.cancelable) e.preventDefault();
 
   const el = e.currentTarget! as HTMLElement;
+  if (!draggables.get(el)) return;
+
   const current = e.targetTouches[0];
   const pos: Position = [current.clientX, current.clientY];
 
@@ -87,6 +111,8 @@ export function moveTouch(e: TouchEvent) {
 
   for (const touch of e.changedTouches) {
     const el = touch.target as HTMLElement;
+    if (!draggables.get(el)) return;
+
     const pos: Position = [touch.clientX, touch.clientY];
     if (dragging.size === 1) move(el, pos, singleDragging[1]);
     else move(el, pos, dragging.get(el)!);
@@ -96,6 +122,7 @@ export function moveTouch(e: TouchEvent) {
 export function endMouse(e: MouseEvent) {
   if (singleDragging === undefined) return;
   // console.log("endmouse");
+  end(singleDragging[0]);
 
   singleDragging = undefined;
   cursorStyle.remove();
@@ -112,6 +139,7 @@ export function endTouch(e: TouchEvent) {
 
   dragging.delete(el);
   if (dragging.size !== 0) singleDragging = dragging.entries().next().value;
+  end(el);
 }
 
 function start(el: HTMLElement, pos: Position, settings: DragSettings) {
@@ -126,36 +154,69 @@ function start(el: HTMLElement, pos: Position, settings: DragSettings) {
     startTranslate: startTranslate as [number, number],
   };
 
-  if (!("ghost" in settings) || settings.ghost === false) {
-    const styles = getComputedStyle(el);
+  if ("ghost" in settings) {
+    const ghost = el.cloneNode(true) as HTMLElement;
+    ghosts.set(el, ghost);
+    if (ghost.id.length) ghost.id += "-ghost";
+    el.insertAdjacentElement("afterend", ghost);
+  }
 
-    if (styles.position !== "fixed" && styles.position !== "absolute") {
-      const marginLeft = parseInt(styles.marginLeft);
-      const marginTop = parseInt(styles.marginTop);
-      state.startTranslate[0] -= marginLeft;
-      state.startTranslate[1] -= marginTop;
-      el.style.position = "absolute";
+  const styles = getComputedStyle(el);
 
-      el.style.translate = `${
-        state.startTranslate[0] + pos[0] - state.startMouse[0]
-      }px ${state.startTranslate[1] + pos[1] - state.startMouse[1]}px`;
+  if (styles.position !== "fixed" && styles.position !== "absolute") {
+    const marginLeft = parseInt(styles.marginLeft);
+    const marginTop = parseInt(styles.marginTop);
+    state.startTranslate[0] -= marginLeft;
+    state.startTranslate[1] -= marginTop;
+    el.style.position = "absolute";
+
+    el.style.translate = `${
+      state.startTranslate[0] + pos[0] - state.startMouse[0]
+    }px ${state.startTranslate[1] + pos[1] - state.startMouse[1]}px`;
+  }
+
+  if (order.length >= 2 && order[order.length - 1][0] !== el) {
+    for (let i = 0; i < order.length - 1; i++) {
+      if (order[i][0] === el) {
+        const temp = order[i];
+        order[i] = order[i + 1];
+        order[i + 1] = temp;
+      }
     }
+
+    const n = order.length - 1;
+    // if we can just swap z-indices
+    if (order.length === 2) {
+      const temp = order[n][1];
+      order[n][1] = order[n - 1][1];
+      order[n - 1][1] = temp;
+    }
+    // if we can decrement the z-index of the previous top element
+    else if (order[n - 1][1] > order[n - 2][1] + 1) {
+      order[n][1] = order[n - 1][1];
+      order[n - 1][1] -= 1;
+    }
+    // if we can decrement the z-index of every other element
+    else if (order[0][1] > orderBase) {
+      order[n][1] = order[n - 1][1];
+      for (let i = 0; i < order.length - 1; i++) {
+        order[i][1] -= 1;
+
+        order[i][0].style.setProperty("z-index", order[i][1] + "", "important");
+      }
+    }
+    // else we must increment the z-index of the new top element
+    else {
+      order[n][1] = order[n - 1][1] + 1;
+    }
+
+    order[n][0].style.setProperty("z-index", order[n][1] + "", "important");
+    order[n - 1][0].style.setProperty(
+      "z-index",
+      order[n - 1][1] + "",
+      "important"
+    );
   }
-
-  const zIndex = parseInt(el.style.zIndex || "0");
-  let found = false;
-
-  for (const el1 of draggables.keys()) {
-    if (el === el1) continue;
-
-    const zIndexOther = parseInt(el1.style.zIndex || "0");
-    if (zIndex + 1 === zIndexOther) {
-      el1.style.zIndex = zIndexOther - 1 + "";
-      found = true;
-    } else if (zIndex === zIndexOther) found = true;
-  }
-
-  if (found) el.style.zIndex = zIndex + 1 + "";
 
   return state;
 }
@@ -164,4 +225,9 @@ function move(el: HTMLElement, pos: Position, state: DragState) {
   el.style.translate = `${
     state.startTranslate[0] + pos[0] - state.startMouse[0]
   }px ${state.startTranslate[1] + pos[1] - state.startMouse[1]}px`;
+}
+
+function end(el: HTMLElement) {
+  const ghost = ghosts.get(el);
+  if (ghost) ghost.remove();
 }
